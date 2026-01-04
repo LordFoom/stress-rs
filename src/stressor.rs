0c11387aafcb99f18f30e6867abfd84531b0d58c
Timothy Spring <TimothyS@bayportfinance.com>
2026-01-04 20:56:28 +0200

    stub commit

--- src/stressor.rs
+++ src/stressor.rs
@@ -1,8 +1,15 @@
 ///Information of the uri we are going to stress,
 ///and how to stress it
-struct Stressor{
+struct Stressor {
     pub description: String,
     pub raw_strength: f16,
 }
 
-pub fn init_stres
+impl Stressor {
+    pub fn new(description: &str, raw_strength: f16) -> Self {
+        Self {
+            description,
+            raw_strength,
+        }
+    }
+}
