74b27bdd7c6bdc2f89ce8a1f784d9b01283bfc6f
LordFoom <lordfoom@gmail.com>
2026-01-08 23:25:47 +0200

    Spawning threads
    Spawning them threads

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use std::thread;
+
 use crate::cli::CliArgs;
+use anyhow::Result;
+use anyhow::anyhow;
 use clap::Parser;
 
 mod cli;
 mod stressor;
 
-fn main() {
+fn main() -> Result<()> {
     let args = CliArgs::parse();
-    args.url;
+    let num_threads = args.threads;
+    let url_to_stress = args.url;
+    let mut vec_threads = Vec::new();
+    for i in 1..=num_threads {
+        let jh = thread::spawn(|| {});
+        vec_threads.push(jh);
+    }
+
+    for th in vec_threads {
+        th.join().map_err(|_| anyhow!("Thread panicked"))?;
+    }
+    Ok(())
 }
