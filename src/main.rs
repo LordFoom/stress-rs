25fa647d409872551378b99562a5873a9ea1f670
LordFoom <lordfoom@gmail.com>
2026-02-03 16:25:34 +0200

    Fix borrowing
    Oh borrow checker, 'tis of thee, checking my locality

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,17 @@ mod stressor;
 fn main() -> Result<()> {
     let args = CliArgs::parse();
     let num_threads = args.threads;
-    // let url_to_stress = args.url;
-    let optional_description = args.description;
+    let url_to_stress = args.url.clone();
+    let optional_description = args.description.clone();
     let mut vec_threads = Vec::new();
     for i in 1..=num_threads {
-        let jh = thread::spawn(|| {
+        let our_url = url_to_stress.clone();
+        let our_desc = optional_description.clone();
+        let jh = thread::spawn(move || {
             let stressor = Stressor::default()
-                .optional_description(optional_description)
+                .optional_description(our_desc)
                 .thread_num(i)
-                .url(args.url);
+                .url(our_url);
         });
         vec_threads.push(jh);
     }
